3d1cddf1ef127201a569ea53a6c2b0a5b821b917
kkubraselcuk <kkubra.selcuk@gmail.com>
2022-08-24 23:24:03 +0300

    Countries-refresh

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -10,6 +10,7 @@ const Countries = () => {
 
     const getData = async ()=>{
         const res = await axios.get('https://restcountries.com/v2/all');
+        console.log(res.data);
         setCountry(res.data);
         setisLoading(false);
     }
@@ -22,20 +23,24 @@ const Countries = () => {
         getData();
     }, []);
 
-    const filteredData = country.filter(IValue => (
-            // capital olmayan verileri almamak için filtreleme yapıyorum.
-            JSON.stringify(IValue).toLowerCase().includes("capital")
-    ));
-
+    
     //Capital alanında arama yapar.
-    const filterData = filteredData.filter(item => (
-        item.capital.toLowerCase().includes(search.toLowerCase())
-    ));
+    const filterData = country.filter(item => {
+        if(typeof(item.capital)=== "undefined"){
+            item.capital=" ";
+            return item.capital;
+        }
+        return item.capital.toLowerCase().includes(search.toLowerCase());
+    })
     
     //Tablodaki veriler üzerinde arama yapar.
-    /*const filterData = filteredData.filter(item => (
-        item.name.toLowerCase().includes(search.toLowerCase()) || item.region.toLowerCase().includes(search.toLowerCase()) || item.capital.toLowerCase().includes(search.toLowerCase())
-    ));*/
+    // const filterData = country.filter(item => {
+    //     if(typeof(item.capital)=== "undefined"){
+    //         item.capital=" ";
+    //         return item.capital;
+    //     }
+    //     return item.name.toLowerCase().includes(search.toLowerCase()) || item.region.toLowerCase().includes(search.toLowerCase()) || item.capital.toLowerCase().includes(search.toLowerCase());
+    // })
 
     return (
         <>
@@ -48,7 +53,7 @@ const Countries = () => {
                     <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30px" height="30px"><path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z" /></svg>
                     <input type="text" value={search} onChange={handleSearch} />
                 </div>
-                <table size="sm" id="table"  class="table  table-borderessed table-striped table-hover align-middle">
+                <table size="sm" id="table"  className="table  table-borderessed table-striped table-hover align-middle">
                     <thead>
                         <tr>
                             <th>Name</th>
